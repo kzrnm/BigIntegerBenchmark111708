dd61a6063f0d4f7cb6f038eb46bba824e44775e1
kzrnm <gengesa@gmail.com>
2025-02-09 00:21:10 +0900

    Run in Actions

--- Console/Program.cs
+++ Console/Program.cs
@@ -3,6 +3,8 @@ extern alias Net9;
 using System.Diagnostics;
 using System.Numerics;
 
+Console.WriteLine(typeof(BigInteger).Assembly);
+
 Run<BigInteger>();
 Run<Net8::System.Numerics.BigInteger>();
 Run<Net9::System.Numerics.BigInteger>();
@@ -12,6 +14,6 @@ static void Run<T>() where T : IBinaryInteger<T>
     var sw = Stopwatch.StartNew();
     var b = (T.One << (1 << 20)).ToString("E9", null);
     sw.Stop();
-    Console.WriteLine(b);
-    Console.WriteLine(sw.Elapsed);
+    Console.WriteLine($"ToString {b}");
+    Console.WriteLine($"{sw.ElapsedMilliseconds} ms");
 }
